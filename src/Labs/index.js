9b173a7e136388aeddb0f9078b5e1a7bab169b6c
aaronkimbrooktec <218adk@gmail.com>
2023-10-16 01:04:29 -0400

    reached end of lab. still need to make fixes

---
+++ src/Labs/index.js
@@ -0,0 +1,21 @@
+import ConditionalOutput from "./a3/ConditionalOutput";
+import Styles from "./a3/Styles";
+import Classes from "./a3/Classes";
+import PathParameters from "./a3/PathParameters";
+import JavaScript from "./a3/JavaScript";
+import TodoList from "./a3/todo/TodoList";
+
+function Assignment3() {
+ return (
+   <div>
+        <h1>Assignment 3</h1>
+        <ConditionalOutput/>
+        <Styles/>
+        <Classes/>
+        <PathParameters/>
+        <JavaScript/>
+        <TodoList/>
+   </div>
+ );
+}
+export default Assignment3;
